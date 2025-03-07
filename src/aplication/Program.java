39115e0db8688c9e182b7fd1d30995bc69b0313f
RaulRodriguesss <santos.raulrodr@gmail.com>
2025-03-07 16:57:41 -0300

    printing game table

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,12 +1,14 @@
 package aplication;
 
 import boardGame.Board;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Board board = new Board(8,8);
+		ChessMatch chessmatch = new ChessMatch();
+		UI.printBoard(chessmatch.getPieces());
 
 	}
 
